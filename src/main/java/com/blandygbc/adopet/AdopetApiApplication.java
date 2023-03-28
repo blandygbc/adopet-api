a06d7130bc6594c6819e4233d180f2e9afd6b783
Gabriel Blandy <gabriel_blandy@hotmail.com>
2023-03-28 14:30:24 -0300

    first commit

---
+++ src/main/java/com/blandygbc/adopet/AdopetApiApplication.java
@@ -0,0 +1,13 @@
+package com.blandygbc.adopet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdopetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdopetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blandygbc/adopet/AdopetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blandygbc.adopet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdopetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
