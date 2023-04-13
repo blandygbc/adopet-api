515e2d36bf4bef2684a9d9b473dacfc581192106
Gabriel <gabrielblandy@gmail.com>
2023-04-12 21:23:17 -0300

    Add pagination to pet list

--- src/main/java/com/blandygbc/adopet/domain/pets/PetRepository.java
+++ src/main/java/com/blandygbc/adopet/domain/pets/PetRepository.java
@@ -1,7 +1,7 @@
 package com.blandygbc.adopet.domain.pets;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,5 +11,5 @@ public interface PetRepository extends JpaRepository<Pet, Long> {
     @Query(value = "DELETE FROM Pet p where p.id = ?1")
     Integer deletePetById(Long id);
 
-    List<Pet> findAllByStatusNot(PetStatus petStatus);
+    Page<Pet> findAllByStatusNot(PetStatus petStatus, Pageable page);
 }
